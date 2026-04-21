93cd9d6e05056f6413e431b0078ac34f6bbc5997
xdh4uv <dhruvnainn@gmail.com>
2026-04-21 17:16:18 +0000

    added cors origin

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "https://vchitr-frontend-git-main-yashs-projects-19769dc0.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
